cfae07886e254850a94a3eac112574857e913cdb
Robert Fleischmann <robert.fleischmann@sinnerschrader.com>
2013-07-03 10:33:05 +0200

    use classNames instead of classList, indent 2 instead of 4 spaces

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,32 +1,35 @@
+/*jslint indent:2 */
+/*global Modernizr, ga */
 (function (d) {
-    var head = d.getElementsByTagName('head')[0],
-        body = d.body,
-        show = function () {
-            setTimeout(function () {
-                body.classList.add('show');
-            }, 700);
-        },
-        hidden = Modernizr.prefixed("hidden", document, false),
-        pagevisibilitychange = (hidden || "").replace('Hidden', 'visibilitychange').toLowerCase();
+  'use strict';
+  var body = d.body,
+    show = function () {
+      setTimeout(function () {
+        body.className += ' show';
+        // body.classList.add('show');
+      }, 700);
+    },
+    hidden = Modernizr.prefixed('hidden', document, false),
+    pagevisibilitychange = (hidden || '').replace('Hidden', 'visibilitychange').toLowerCase();
 
-    d.addEventListener('DOMContentLoaded', function () {
-        // iphone 5+ viewport fix
-        if (window.screen.height === 568) {
-            document.querySelector("meta[name=viewport]").content = "width=320.1";
-        }
-        // trigger loading animation
-        if (!!hidden && d[hidden]) {
-            d.addEventListener(pagevisibilitychange, show, false);
-        } else {
-            show();
-        }
-    });
+  d.addEventListener('DOMContentLoaded', function () {
+    // iphone 5+ viewport fix
+    if (window.screen.height === 568) {
+      document.querySelector('meta[name=viewport]').content = 'width=320.1';
+    }
+    // trigger loading animation
+    if (!!hidden && d[hidden]) {
+      d.addEventListener(pagevisibilitychange, show, false);
+    } else {
+      show();
+    }
+  });
 
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-34062482-2', 'robert-fleischmann.de');
-    ga('send', 'pageview');
+  ga('create', 'UA-34062482-2', 'robert-fleischmann.de');
+  ga('send', 'pageview');
 }(document));
